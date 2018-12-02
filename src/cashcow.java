c00ddb377998b3fed161f3f4b3888c1c1e101159
Jeremy Campbell <jcampbell2@harding.edu>
2018-12-01 18:01:33 -0600

    Passed all test cases, still looks kinda horrible though.

--- src/cashcow.java
+++ src/cashcow.java
@@ -2,6 +2,9 @@
 // cashcow.java
 // Jeremy Campbell
 // Project 10 of Applied Algorithms
+// I am unsatisfied with a lot of my coding practices
+// in this project but I cannot afford to rewrite at
+// the moment.
 // ---------------------------------------------------
 
 import java.awt.*;
@@ -9,7 +12,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -23,6 +25,11 @@ public class cashcow {
             this.column = column - 'a';
             this.row = row - 1;
         }
+
+        @Override
+        public String toString() {
+            return "(" + row + ", " + column + ")";
+        }
     }
 
     private static class GameBoard {
@@ -30,7 +37,6 @@ public class cashcow {
         private int COLS = 10;
         private Character[][] board;
         private ArrayList<Move> moves;
-        HashSet<Point> markedSet;
         private int numMoves;
         private int totalCharacters = ROWS * COLS;
 
@@ -73,111 +79,146 @@ public class cashcow {
         private int clearCluster(Move current) {
             int slotsCleared = 0;
             Stack<Point> points = new Stack<>();
-            markedSet = new HashSet<>();
-            ArrayList<Point> pointsTested = new ArrayList<>();
+            Character[][] scratch = board.clone();
             points.push(new Point(current.row, current.column));
+            Character toFind = board[current.row][current.column];
+
+            System.out.println("Gameboard before move : " + current);
+            printGameState();
 
             while(!points.empty()) {
                 Point currentPoint = points.pop();
 
                 // Check North
                 slotsCleared += testPoint(new Point(currentPoint.x - 1, currentPoint.y),
-                        currentPoint, pointsTested, points);
+                        scratch, toFind, points);
 
                 // Check East
                 slotsCleared += testPoint(new Point(currentPoint.x, currentPoint.y + 1),
-                        currentPoint, pointsTested, points);
+                        scratch, toFind, points);
 
                 // Check South
                 slotsCleared += testPoint(new Point(currentPoint.x + 1, currentPoint.y),
-                        currentPoint, pointsTested, points);
+                        scratch, toFind, points);
 
                 // Check West
                 slotsCleared += testPoint(new Point(currentPoint.x, currentPoint.y - 1),
-                        currentPoint, pointsTested, points);
+                        scratch, toFind, points);
 
             }
 
-            if (slotsCleared > 3) {
+            if (slotsCleared >= 3) {
+                board = scratch;
+                System.out.println("Gameboard before compaction: ");
+                printGameState();
                 compactBoard();
             }else {
                 slotsCleared = 0;
-                for (Point p : pointsTested) {
-                    markedSet.remove(p);
-                }
             }
-            pointsTested.clear();
+
+            System.out.println("Gameboard after move : " + current);
+            printGameState();
+
             return slotsCleared;
         }
 
-        private int testPoint(Point pToTest, Point pOld, ArrayList<Point> pointsTested, Stack<Point> points) {
+        private int testPoint(Point pToTest, Character[][] gameboard, Character toFind, Stack<Point> points) {
             int cleared = 0;
             // If it's a valid move
             if (pToTest.x < ROWS && pToTest.x >= 0 && pToTest.y < COLS && pToTest.y >= 0) {
                 // If the point hasn't already been tested and it is the same color
-                if (!markedSet.contains(pToTest) &&
-                        board[pToTest.x][pToTest.y].equals(board[pOld.x][pOld.y])){
+                if (gameboard[pToTest.x][pToTest.y] != null &&
+                        gameboard[pToTest.x][pToTest.y].equals(toFind)){
                     points.push(pToTest);
-
-                    markedSet.add(pToTest);
-                    pointsTested.add(pToTest);
+                    gameboard[pToTest.x][pToTest.y] = null;
                     cleared = 1;
                 }
             }
             return cleared;
         }
 
-        private Character[][] copyBoard() {
-            Character[][] scratch = new Character[ROWS][COLS];
-            for (int i = 0; i < ROWS; i++) {
-                for (int j = 0; j < COLS; j++) {
-                    scratch[i][j] = board[i][j];
-                }
-            }
-            return scratch;
+        private void compactBoard() {
+            compactBoardUp();
+            System.out.println("Gameboard after moving up: ");
+            printGameState();
+            compactBoardLeft();
         }
 
-        private void compactBoard() {
-            // Compress the board downward
-            for (int i = ROWS - 2; i > 0; i--) {
-                for (int j = COLS - 1; j >= 0; j--) {
-                    Point current = new Point(i, j);
-                    Point above = new Point(i - 1, j);
-                    while (markedSet.contains(current) && !markedSet.contains(above)) {
-                        markedSet.remove(current);
-                        board[i][j] = board[i - 1][j];
-                        markedSet.add(above);
+        private void compactBoardUp() {
+            // Compress the board downward (which is actually upward in the way I've stored the data)
+            for (int i = 0; i < COLS; i++) {
+                for (int j = 0; j < ROWS - 1; j++) {
+                    int column = i;
+                    int row = j;
+                    boolean noneFoundAbove = false;
+
+                    while (!noneFoundAbove && board[row][column] == null) {
+                        row++;
+                        if (row >= ROWS) {
+                            noneFoundAbove = true;
+                        }
+                    }
+                    if (!noneFoundAbove && board[j][i] == null) {
+                        // move was valid, move the value down
+                        board[j][i] = board[row][column];
+                        board[row][column] = null;
                     }
                 }
             }
+        }
 
-            // Compress the board leftward, row by row
+        private void compactBoardLeft() {
+            // Compress the board leftward, column by column
             for (int i = 0; i < COLS - 1; i++) {
-                boolean allInvalid = true;
-                for (int j = 0; j < ROWS; j++) {
-                    if (!markedSet.contains(new Point(j, i))) {
-                        allInvalid = false;
-                        break;
+                boolean currentColNull = checkIfColNull(i);
+
+                // Go until either I find a column that is not null or there are no more columns to check
+                if (currentColNull) {
+                    int column = i + 1;
+                    boolean nextColNull = true;
+                    while (column < COLS && nextColNull) {
+                        nextColNull = checkIfColNull(column);
+                        column++;
+                    }
+                    if (!nextColNull) {
+                        column--;
+                        swapCols(i, column);
                     }
                 }
-                if (allInvalid) {
-                    for (int j = 0; j < ROWS - 1; j++) {
-                        markedSet.remove(new Point(j, i));
-                        board[j][i] = board[j + 1][i];
-                        markedSet.add(new Point(j + 1, i));
-                    }
+            }
+        }
+
+        private boolean checkIfColNull(int col) {
+            boolean allNull = true;
+            for (int j = 0; j < ROWS; j++) {
+                if (board[j][col] != null) {
+                    allNull = false;
+                    break;
                 }
+            }
+            return allNull;
+        }
+
+        private void swapCols(int first, int second) {
+            for (int i = 0; i < ROWS; i++) {
+                board[i][first] = board[i][second];
+                board[i][second] = null;
             }
         }
 
         // Debug function
-        private void printBoard() {
+        private void printGameState() {
+            System.out.println("--------------------------");
             for (Character[] row : board) {
                 for (Character c : row) {
-                    System.out.print(c);
+                    if (c == null)
+                        System.out.print(".");
+                    else
+                        System.out.print(c);
                 }
                 System.out.println();
             }
+            System.out.println("--------------------------");
         }
 
         public void printScore(PrintWriter fout) {
